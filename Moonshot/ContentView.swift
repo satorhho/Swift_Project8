0e609daf2b82851ebd9236da1adc11710b9cd049
satorhho <lbriones@gbox.adnu.edu.ph>
2020-05-01 18:57:02 +0800

    Initial commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Lance Kent Briones on 5/1/20.
+//  Copyright © 2020 Lance Kent Briones. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
