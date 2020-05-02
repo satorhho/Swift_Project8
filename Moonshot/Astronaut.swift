e1aad5e6505c94f97d81641e975053ec45662c7f
satorhho <lbriones@gbox.adnu.edu.ph>
2020-05-02 22:15:55 +0800

    fixed View

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Lance Kent Briones on 5/2/20.
+//  Copyright © 2020 Lance Kent Briones. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
